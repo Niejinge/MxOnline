ce96839aa75e837d70b5a992f03439df1fb3b45a
Niejinge <niejinge6@163.com>
2018-03-20 19:42:05 +0800

    刚刚完成用户注册找回密码

---
+++ apps/users/forms.py
@@ -0,0 +1,31 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'goldbro'
+__date__ = '2018/3/18 17:13'
+from django import forms
+from captcha.fields import CaptchaField
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, min_length=6)
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(required=True, min_length=6)
+    captcha = CaptchaField(error_messages={"invalid": u"验证码错误"})
+
+
+class ForgetForm(forms.Form):
+    email = forms.EmailField(required=True)
+    captcha = CaptchaField(error_messages={"invalid": u"验证码错误"})
+
+
+class ModifyPwdForm(forms.Form):
+    password1 = forms.CharField(required=True, min_length=6)
+    password2 = forms.CharField(required=True, min_length=6)
+
+
+
+
+
---
+++ apps/utils/__init__.py
@@ -0,0 +1,3 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'goldbro'
+__date__ = '2018/3/19 9:41'
